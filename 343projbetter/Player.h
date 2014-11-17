5fb87fe1beaf75529abe32090eb4459926a786bb
Joe McCully <jmccully88@yahoo.com>
2014-11-16 22:02:35 -0800

    My proj

---
+++ 343projbetter/Player.h
@@ -0,0 +1,9 @@
+#ifndef __PLAYER_H
+#define __PLAYER_H
+
+class Player{
+public:
+	virtual ~Player() {}
+
+};
+#endif
---
+++ 343projbetter/leveltest.cpp
@@ -0,0 +1,85 @@
+#include "level.h"
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+#include <iostream>
+#include <fstream>
+using namespace::std;
+void RedirectIOToConsole();
+int main()
+{
+	RedirectIOToConsole();
+	Level level;
+	level.LoadFromFile("testmap.tmx");
+
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(800, 600), "Level.h test");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		level.Draw(window);
+		window.display();
+	}
+
+	return 0;
+}
+static const WORD MAX_CONSOLE_LINES = 500;
+void RedirectIOToConsole()
+{
+	int hConHandle;
+	long lStdHandle;
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	FILE *fp;
+
+	// allocate a console for this app
+	AllocConsole();
+
+	// set the screen buffer to be big enough to let us scroll text
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
+	coninfo.dwSize.Y = MAX_CONSOLE_LINES;
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
+
+	// redirect unbuffered STDOUT to the console
+	lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
+	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+
+	fp = _fdopen(hConHandle, "w");
+
+	*stdout = *fp;
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	// redirect unbuffered STDIN to the console
+
+	lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
+	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+
+	fp = _fdopen(hConHandle, "r");
+	*stdin = *fp;
+	setvbuf(stdin, NULL, _IONBF, 0);
+
+	// redirect unbuffered STDERR to the console
+	lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
+	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+
+	fp = _fdopen(hConHandle, "w");
+
+	*stderr = *fp;
+
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	// make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
+	// point to console as well
+	ios::sync_with_stdio();
+}
